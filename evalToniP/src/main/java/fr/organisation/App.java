fa6f76bfb4964f43d7570e4ac722bfa41cc03dc9
ToniPad <padovan.toni@gmail.com>
2020-02-12 17:09:53 +0100

    commit de fin

--- evalToniP/src/main/java/fr/organisation/App.java
+++ evalToniP/src/main/java/fr/organisation/App.java
@@ -1,14 +1,12 @@
 package fr.organisation;
 
-/**
- * Hello world!
+/* Hello world!
  */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
+    /* Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
